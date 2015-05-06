84538eb4cb6420aded50e0ab99396b9d949bd016
Gil Azaria <me@gilazaria.com>
2015-05-06 15:34:43 +1000

    Added an Extractor class with tests equivalent to SimpleExtractSpec

---
+++ src/main/scala/tools/ambitious/pdfextractiontoolkit/extraction/Extractor.scala
@@ -0,0 +1,75 @@
+package tools.ambitious.pdfextractiontoolkit.extraction
+
+import org.apache.pdfbox.pdmodel.PDDocument
+import technology.tabula.ObjectExtractor
+import technology.tabula.extractors.BasicExtractionAlgorithm
+import technology.tabula
+import tools.ambitious.pdfextractiontoolkit.model.constraints.{PageNumberConstraint, Constraint}
+import tools.ambitious.pdfextractiontoolkit.model._
+
+import scala.collection.JavaConverters._
+
+class Extractor {
+  private var documents: List[Document] = Nil
+  private var stencil: Stencil = null
+
+  def addDocument(document: Document) =
+    addDocuments(List(document))
+
+  def addDocuments(documents: List[Document]) =
+    this.documents = this.documents ++ documents
+
+  def applyStencil(stencil: Stencil) =
+    this.stencil = stencil
+
+  def extractTables() = {
+    for (window: Window <- stencil.windows)
+      extractTableFromWindow(window)
+  }
+
+  private def extractTableFromWindow(window: Window) = {
+    for (constraint: Constraint <- window.constraints) {
+
+      /**
+       * The following is not 'clean code' and should be refactored at will.
+       */
+
+      constraint match {
+        case pageNumberConstraint: PageNumberConstraint =>
+          val pageNumber: Int = pageNumberConstraint.pageNumber
+
+          for (document: Document <- documents) {
+            val page: Page = document.getPage(pageNumber)
+            val pageAsPDDocument: PDDocument = page.asPDDocument
+
+            val objectExtractor = new ObjectExtractor(pageAsPDDocument)
+            val wholePage: tabula.Page = objectExtractor.extract(1)
+            val tablePageArea = wholePage.getArea(
+              window.location.y.toFloat,
+              window.location.x.toFloat,
+              (window.location.y + window.size.height).toFloat,
+              (window.location.x + window.size.width).toFloat)
+
+            val extractionAlgorithm = new BasicExtractionAlgorithm
+            val tabulaTable = extractionAlgorithm.extract(tablePageArea).get(0)
+
+            val table: Table = new Table
+
+            for (tabulaRow <- tabulaTable.getRows.asScala) {
+              val row: Row = new Row
+              for (tabulaCell <- tabulaRow.asScala) {
+                val cell: Cell = new Cell(tabulaCell.getText.trim)
+                row.addCell(cell)
+              }
+              table.addRow(row)
+            }
+
+            document.addTable(table)
+          }
+        case _ =>
+          throw new Exception("Unknown constraint type!")
+      }
+    }
+
+  }
+}
---
+++ src/test/scala/tools/ambitious/pdfextractiontoolkit/extractor/ExtractorSpec.scala
@@ -0,0 +1,54 @@
+package tools.ambitious.pdfextractiontoolkit.extractor
+
+import org.scalatest.FreeSpec
+import tools.ambitious.pdfextractiontoolkit.extraction.Extractor
+import tools.ambitious.pdfextractiontoolkit.model.constraints.PageNumberConstraint
+import tools.ambitious.pdfextractiontoolkit.model.geometry.{PositivePoint, Size}
+import tools.ambitious.pdfextractiontoolkit.model._
+
+class ExtractorSpec extends FreeSpec {
+  val samplePDFPath = getClass.getResource("/simplePDFs/SimpleTest1Table.pdf")
+
+  "An Extractor" - {
+    val extractor: Extractor = new Extractor
+
+    "should be able to add a document" in {
+      val document: Document = Document.fromPDFPath(samplePDFPath)
+      extractor.addDocument(document)
+    }
+
+    "should be able to apply a stencil" in {
+      val stencil: Stencil = new Stencil
+      val window: Window = new Window(new PositivePoint(81, 108), new Size(224, 204))
+      val pageNumberConstraint: PageNumberConstraint = new PageNumberConstraint(1)
+      window.addConstraint(pageNumberConstraint)
+      stencil.addWindow(window)
+
+      extractor.applyStencil(stencil)
+    }
+  }
+
+  "An Extractor with an added document and valid applied stencil" - {
+    val extractor: Extractor = new Extractor
+
+    val document: Document = Document.fromPDFPath(samplePDFPath)
+    extractor.addDocument(document)
+
+    val pageNumberConstraint: PageNumberConstraint = new PageNumberConstraint(1)
+    val window: Window = new Window(new PositivePoint(108, 81), new Size(204, 224))
+    window.addConstraint(pageNumberConstraint)
+
+    val stencil: Stencil = new Stencil
+    stencil.addWindow(window)
+
+    extractor.applyStencil(stencil)
+
+    "should be able to extract the table and have the value 10 in its top left cell" in {
+      extractor.extractTables()
+      val table: Table = document.tables.head
+      val cell: Cell = table.getCell(1,1)
+
+      assert(cell.text == "10")
+    }
+  }
+}
