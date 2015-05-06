3cd5f68dc8a0b35edc96d52256934cda34fc766c
Gil Azaria <me@gilazaria.com>
2015-05-06 15:26:52 +1000

    Added toString method, a second addCell method (taking the cell text) and associated tests

--- src/main/scala/tools/ambitious/pdfextractiontoolkit/model/Row.scala
+++ src/main/scala/tools/ambitious/pdfextractiontoolkit/model/Row.scala
@@ -3,11 +3,16 @@ package tools.ambitious.pdfextractiontoolkit.model
 class Row {
   private var cells: List[Cell] = Nil
 
-  def addCell(cell: Cell) = {
+  def addCell(cell: Cell) =
     cells = cells ++ List(cell)
-  }
 
-  def getCell(i: Int) = {
+  def addCell(cellText: String): Unit =
+    addCell(new Cell(cellText))
+
+  def getCell(i: Int) =
     cells(i-1)
-  }
+
+  override def toString: String =
+    cells.map(cell => cell.text).mkString(",")
+
 }
--- src/test/scala/tools/ambitious/pdfextractiontoolkit/model/RowSpec.scala
+++ src/test/scala/tools/ambitious/pdfextractiontoolkit/model/RowSpec.scala
@@ -15,4 +15,27 @@ class RowSpec extends FreeSpec {
       }
     }
   }
+
+  "A row" - {
+    var row: Row = new Row
+
+    "that adds a cell using text" - {
+      row.addCell("test")
+
+      "should have the first cell contain the text 'test" in {
+        assert(row.getCell(1).text == "test")
+      }
+    }
+  }
+
+  "A row containing the entries 1, 2 and 3" - {
+    var row: Row = new Row
+    row.addCell("1")
+    row.addCell("2")
+    row.addCell("3")
+
+    "should return '1,2,3' when converted to String" in {
+      assert(row.toString == "1,2,3")
+    }
+  }
 }
