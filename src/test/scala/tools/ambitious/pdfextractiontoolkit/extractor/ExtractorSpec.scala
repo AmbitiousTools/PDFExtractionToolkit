105b896693d3b6f21865714af08b2200fb91f35f
Gil Azaria <me@gilazaria.com>
2015-05-08 16:14:59 +1000

    Added CSV files for the simple test table PDFs, and modified Extractor tests to check entire tables instead of a single cell

--- src/test/scala/tools/ambitious/pdfextractiontoolkit/extractor/ExtractorSpec.scala
+++ src/test/scala/tools/ambitious/pdfextractiontoolkit/extractor/ExtractorSpec.scala
@@ -5,15 +5,19 @@ import tools.ambitious.pdfextractiontoolkit.extraction.Extractor
 import tools.ambitious.pdfextractiontoolkit.model.constraints.{FirstOccurrenceOfStringInWindowConstraint, PageNumberConstraint}
 import tools.ambitious.pdfextractiontoolkit.model._
 import tools.ambitious.pdfextractiontoolkit.model.geometry.{Size, PositivePoint}
+import tools.ambitious.pdfextractiontoolkit.util.CSVUtil
 
 import scala.collection.immutable.ListMap
 
 class ExtractorSpec extends FreeSpec {
-  val simpleTest1TablePath = getClass.getResource("/simplePDFs/SimpleTest1Table.pdf")
-  val simpleTest2Tables2TitlePath = getClass.getResource("/simplePDFs/SimpleTest2Tables1Title.pdf")
+  val simpleTest1TableURL = getClass.getResource("/simplePDFs/SimpleTest1Table.pdf")
+  val simpleTest1TableCSVURL = getClass.getResource("/simpleCSVs/SimpleTest1Table.csv")
+
+  val simpleTest2Tables2TitleURL = getClass.getResource("/simplePDFs/SimpleTest2Tables1Title.pdf")
+  val simpleTest2Tables2TitlePage2CSVURL = getClass.getResource("/simpleCSVs/SimpleTest2Tables1TitlePage2.csv")
 
   "An Extractor with a document and valid stencil" - {
-    val document: Document = Document.fromPDFPath(simpleTest1TablePath)
+    val document: Document = Document.fromPDFPath(simpleTest1TableURL)
 
     val window: Window = Window.fromAbsoluteCoordinates(108, 81, 312, 305)
     val tracker: ConstraintTracker = new ConstraintTracker(new PageNumberConstraint(1))
@@ -22,19 +26,19 @@ class ExtractorSpec extends FreeSpec {
 
     val extractor: Extractor = Extractor.fromStencilAndDocument(stencil, document)
 
-    "should be able to extract the table and have the value 10 in its top left cell" in {
+    "should be able to extract the table and have it match the values from it's corresponding CSV file" in {
       val tableMap: Map[Document, Table] = extractor.extractTables
       document.close()
 
       val table: Table = tableMap(document)
-      val cell: Cell = table.getCell(1,1)
+      val tableFromCSV: Table = CSVUtil.tableFromURL(simpleTest1TableCSVURL)
 
-      assert(cell.text == "10")
+      assert(table == tableFromCSV)
     }
   }
 
   "An Extractor with one document and a stencil using the FirstOccurrenceOfStringInWindow constraint" - {
-    val document: Document = Document.fromPDFPath(simpleTest2Tables2TitlePath)
+    val document: Document = Document.fromPDFPath(simpleTest2Tables2TitleURL)
 
     val constraintWindow: Window = new Window(new PositivePoint(185.38, 165.62), new Size(112.64, 16.16))
     val constraint = new FirstOccurrenceOfStringInWindowConstraint("An example Title", constraintWindow)
@@ -46,14 +50,14 @@ class ExtractorSpec extends FreeSpec {
 
     val extractor: Extractor = Extractor.fromStencilAndDocument(stencil, document)
 
-    "should be able to extract the table and have the value 4 in its top left cell" in {
+    "should be able to extract the table and have it match the values from it's corresponding CSV file" in {
       val tableMap: Map[Document, Table] = extractor.extractTables
       document.close()
 
       val table: Table = tableMap(document)
-      val cell: Cell = table.getCell(1,1)
+      val tableFromCSV: Table = CSVUtil.tableFromURL(simpleTest2Tables2TitlePage2CSVURL)
 
-      assert(cell.text == "4")
+      assert(table == tableFromCSV)
     }
   }
 }
