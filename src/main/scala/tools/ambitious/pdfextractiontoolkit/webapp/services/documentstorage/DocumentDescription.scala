1559c734591ccee256bce516ac4b9c135d821826
gavia <me@gilazaria.com>
2015-10-28 19:01:13 +1100

    - Created a file store implementation with tests - Changed document identifier to store hash in base16 - Added a mime type to the document description

--- src/main/scala/tools/ambitious/pdfextractiontoolkit/webapp/services/documentstorage/DocumentDescription.scala
+++ src/main/scala/tools/ambitious/pdfextractiontoolkit/webapp/services/documentstorage/DocumentDescription.scala
@@ -1,15 +1,25 @@
 package tools.ambitious.pdfextractiontoolkit.webapp.services.documentstorage
 
+import spray.http.MediaType
+
 /**
  * A description of a document in the [[DocumentLibrary]], containing information that would be useful to a user.
  */
 case class DocumentDescription protected (title: String,
-                                          description: Option[String]) {
+                                          description: Option[String],
+                                          mediaType: MediaType = MediaType.custom("application/octet-stream")) {
 }
 
 object DocumentDescription {
+  def withTitleAndDescriptionAndMediaType(title: String, description: String, mediaType: MediaType): DocumentDescription =
+    new DocumentDescription(title, Some(description), mediaType)
+
+  def withTitleAndMediaType(title: String, mediaType: MediaType): DocumentDescription =
+    new DocumentDescription(title, None, mediaType)
+
   def withTitleAndDescription(title: String, description: String): DocumentDescription =
     new DocumentDescription(title, Some(description))
 
-  def withTitle(title: String): DocumentDescription = new DocumentDescription(title, None)
+  def withTitle(title: String): DocumentDescription =
+    new DocumentDescription(title, None)
 }
