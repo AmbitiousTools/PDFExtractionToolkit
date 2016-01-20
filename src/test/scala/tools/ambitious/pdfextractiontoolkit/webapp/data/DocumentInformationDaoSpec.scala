b883dba56a57bc4e708f4b0ca0a078dea75b2585
Timothy McCarthy <timothy.michael.mccarthy@gmail.com>
2016-01-20 18:46:29 +1100

    Fixing tests  - Refactor the DocumentInformationDaoSpec

--- src/test/scala/tools/ambitious/pdfextractiontoolkit/webapp/data/DocumentInformationDaoSpec.scala
+++ src/test/scala/tools/ambitious/pdfextractiontoolkit/webapp/data/DocumentInformationDaoSpec.scala
@@ -1,58 +1,63 @@
 package tools.ambitious.pdfextractiontoolkit.webapp.data
 
-import org.scalatest.FreeSpec
 import slick.driver.SQLiteDriver.api._
 import slick.lifted.{Query, TableQuery}
 import spray.http.MediaTypes
+import tools.ambitious.pdfextractiontoolkit.AmbitiousToolsSpec
 import tools.ambitious.pdfextractiontoolkit.webapp.data.model.{Document, Documents}
 import tools.ambitious.pdfextractiontoolkit.webapp.services.documentstorage.{DocumentDescription, DocumentIdentifier}
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
-class DocumentInformationDaoSpec extends FreeSpec {
+class DocumentInformationDaoSpec extends AmbitiousToolsSpec {
 
   val rootDao: RootDAO = DAOTestUtils.constructCleanRootDAO
   val dao = DocumentInformationDao.forRootDao(rootDao)
 
-  "when a document is stored" - {
+  val documentID: DocumentIdentifier = constructDocumentId
 
-    val hash: String = "425A"
+  storeDocumentInDatabase(documentID)
+
+  val allDocuments = retrieveAllDocsForID(documentID)
+
+  "the document information dao" should "write one database row per stored document ID" in {
+    assert(allDocuments.length == 1)
+  }
+
+  it should "store the document's hash" in {
+    assert(allDocuments.head.hash == documentID.hash)
+  }
+
+  it should "store the document's title" in {
+    assert(allDocuments.head.title == documentID.description.title)
+  }
+
+  it should "store the document's description" in {
+    assert(documentID.description.description.contains(allDocuments.head.description))
+  }
+
+  it should "store the document's media type" in {
+    assert(allDocuments.head.mediaType == documentID.description.mediaType.toString())
+  }
+
+  def constructDocumentId: DocumentIdentifier = {
+    val testHash: String = "425A"
+
     val description: DocumentDescription = DocumentDescription
       .withTitleAndDescriptionAndMediaType("testDoc", "A test document", MediaTypes.`application/pdf`)
 
-    val documentID: DocumentIdentifier = DocumentIdentifier.withHashAndDescription(hash, description)
+    DocumentIdentifier.withHashAndDescription(testHash, description)
+  }
+
+  def storeDocumentInDatabase(documentID: DocumentIdentifier): Unit = {
     Await.result(dao.storeDocumentID(documentID), 30.seconds)
+  }
 
-    "the table" - {
-      val tableQuery: TableQuery[Documents] = new TableQuery(new Documents(_))
-      val query: Query[Documents, Document, Seq] = tableQuery.map(row => row)
+  def retrieveAllDocsForID(documentID: DocumentIdentifier): Seq[Document] = {
+    val tableQuery: TableQuery[Documents] = new TableQuery(new Documents(_))
 
-      val allDocuments: Seq[Document] = Await.result(rootDao.database.run(query.result), 30.seconds)
-
-      "has one row" in {
-        assert(allDocuments.length == 1)
-      }
-
-      "row" - {
-        val theRow: Document = allDocuments.head
-
-        "has the same hash" in {
-          assert(theRow.hash == documentID.hash)
-        }
-
-        "has the same title" in {
-          assert(theRow.title == documentID.description.title)
-        }
-
-        "has the same description" in {
-          assert(documentID.description.description.contains(theRow.description))
-        }
-
-        "has the same media type" in {
-          assert(theRow.mediaType == documentID.description.mediaType.toString())
-        }
-      }
-    }
+    val query: Query[Documents, Document, Seq] = tableQuery.map(row => row)
+    Await.result(rootDao.database.run(query.result), 30.seconds)
   }
 }
